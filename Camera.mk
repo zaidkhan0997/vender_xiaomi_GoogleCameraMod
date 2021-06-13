ec92bcba4828b082e70e7ec08cafea2b4ec9bf69
zaidkhan0997 <kzaid0997@gmail.com>
2021-06-13 15:57:21 +0530

    GoogleCamera: Add Gcam Permissions
    Signed-off-by: zaidkhan0997 <kzaid0997@gmail.com>

--- Camera.mk
+++ Camera.mk
@@ -1,2 +1,5 @@
 PRODUCT_PACKAGES += \
-    GoogleCamera 
+    GoogleCamera
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/system/etc/permissions/com.google.android.GoogleCameraEng.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/com.google.android.GoogleCameraEng.xml
