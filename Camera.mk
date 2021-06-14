8db04a66daba6d6fa7462976427a897dae00e561
zaidkhan0997 <kzaid0997@gmail.com>
2021-06-14 06:35:36 +0530

    Revert "GoogleCamera: Add Gcam Permissions"
    This reverts commit ec92bcba4828b082e70e7ec08cafea2b4ec9bf69.

--- Camera.mk
+++ Camera.mk
@@ -1,5 +1,2 @@
 PRODUCT_PACKAGES += \
-    GoogleCamera
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/system/etc/permissions/com.google.android.GoogleCameraEng.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/com.google.android.GoogleCameraEng.xml
+    GoogleCamera 
